94cc97f0b1eedef34b87488b51f9b61736d3732f
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-07-18 19:17:46 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 6:00PM"
+          content="Nice blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 18:00PM"
+          content="Good post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Yesterday at 13:00PM"
+          content="Simple and Easy to use!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+// const App = () => {
+//   return (
+//     <div className="ui container comments">
+//       <div className="comment">
+//         <a href="/" className="avatar">
+//           <img alt="avatar" src={faker.image.avatar()} />
+//         </a>
+//         <div className="content">
+//           <a href="/" className="author">
+//             Sam
+//           </a>
+//           <div className="metadata">
+//             <span classNAme="date">Today at 6:00pm</span>
+//           </div>
+//           <div className="text">Nice blog post!</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+ReactDOM.render(<App />, document.querySelector('#root'));
